f7fbaed8761cf9bb2fa2b36d04f70b130dac64f5
Arnes <stu209750@mail.uni-kiel.de>
2023-10-10 00:38:58 +0200

    First Database query done

--- startServer.js
+++ startServer.js
@@ -9,7 +9,7 @@
 import { createServer } from 'http'
 
 //The Data we will be able to send back
-import { getPersonalData, getAllPersonalData } from 'controllers/personalDataController.js'
+import { getPersonalData, getAllPersonalData } from '/Arnes/Website/server/myownRESTAPI/controllers/personalDataController.js'
 
 //Endpoint that handles the actual request
 const server = createServer((req, res) => {
