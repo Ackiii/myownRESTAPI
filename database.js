e152d590cf29472157adf9535b62b4ca6ea6ea80
Arnes <stu209750@mail.uni-kiel.de>
2023-10-12 23:35:32 +0200

    Fixed import problems on local pc

--- database.js
+++ database.js
@@ -2,11 +2,14 @@
  * This file handles the connection of the Database
  * credentials are stored seperatly and are not in the git Repository.
  */
-
+import fs from 'fs';
 import credentials from './credentials.js';
 import { createConnection } from 'mysql';
 const tableName = "apidata";
 const { hostid, username, userpassword } = credentials;
+const logFilePath = 'error.log';
+const errorMessage = 'Error: Connection to the database failed.';
+
 
 const connection = createConnection({
     host: hostid,
@@ -17,7 +20,13 @@ const connection = createConnection({
 
 async function searchForData(keyword){
     connection.query(`SELECT ? FROM ?`,[keyword,tableName], async (err,res)=>{
-        //TODO:
+        fs.appendFile(logFilePath, errorMessage + '\n', (err) => {
+            if (err) {
+              console.error('Error writing to the log file:', err);
+            } else {
+              console.log('Error message logged to', logFilePath);
+            }
+        });
         console.log(res);
         return res;
     })
