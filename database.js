ee6742052d00a7389c076037231a3157e44273eb
Arnes <stu209750@mail.uni-kiel.de>
2023-10-12 23:46:01 +0200

    Changed to mysql2 package, because there were problems with the client logging in

--- database.js
+++ database.js
@@ -4,7 +4,7 @@
  */
 import fs from 'fs';
 import credentials from './credentials.js';
-import { createConnection } from 'mysql';
+import { createConnection } from 'mysql2';
 const tableName = "apidata";
 const { hostid, username, userpassword } = credentials;
 const logFilePath = 'error.log';
