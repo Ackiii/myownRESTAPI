0818693951851657d0c2bd256e2bc0d056ee5a5d
Arnes <stu209750@mail.uni-kiel.de>
2023-10-12 23:39:52 +0200

    Fixed import problems on local pc

---
+++ testDatabase.js
@@ -0,0 +1,7 @@
+import f from 'database.js';
+
+const { searchForData } = f;
+
+const x = "prename";
+
+searchForData(x).then((result) => { console.log("Search Result:", result) }).catch((err) => { console.log("Error:", err) });
