4baa23b6d7092fa4a3e5e549d878a66d858a0f41
Arnes <stu209750@mail.uni-kiel.de>
2023-11-07 17:41:25 +0100

    Changed from connection to Pool, to prevent auto-shutdowns

--- database.js
+++ database.js
@@ -3,53 +3,55 @@
  * credentials are stored seperatly and are not in the git Repository.
  */
 import credentials from './credentials.js';
-import { createConnection } from 'mysql2';
+import mysql from 'mysql2';
 const tableName = 'apidata';
 const { hostid, username, userpassword } = credentials;
 const databaseName = 'ARNESRESTAPI';
 
-const connection = createConnection({
+const pool = mysql.createPool({
     host: hostid,
     user: username,
     password: userpassword,
     database: databaseName,
+    waitForConnections: true,
     connectionLimit: 10,
+    queueLimit: 0
 })
 
 function searchForData(keyword) {
-  return new Promise((resolve, reject) => {
-      connection.query(`SELECT ${keyword} FROM ${tableName} WHERE id = 1`, (err, res) => {
-          if (err) {
-              reject(err);
-              return;
-          }
-          resolve(res);
-      });
-  });
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT ${keyword} FROM ${tableName} WHERE id = 1`, (err, res) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(res);
+        });
+    });
 }
 
 function searchForAllData() {
-  return new Promise((resolve, reject) => {
-      connection.query(`SELECT prename,name,fullname,birthdate,url,favanimal,obsession,post FROM ${tableName} WHERE id = 1`, (err, res) => {
-          if (err) {
-              reject(err);
-              return;
-          }
-          resolve(res);
-      });
-  });
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT prename,name,fullname,birthdate,url,favanimal,obsession,post FROM ${tableName} WHERE id = 1`, (err, res) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(res);
+        });
+    });
 }
 
 function updateValue(value) {
-  return new Promise((resolve, reject) => {
-      connection.query(`UPDATE ${tableName} SET post = ${value} WHERE id = 1`, (err, res) => {
-          if (err) {
-              reject(err);
-              return;
-          }
-          resolve(res);
-      });
-  });
+    return new Promise((resolve, reject) => {
+        pool.query(`UPDATE ${tableName} SET post = ${value} WHERE id = 1`, (err, res) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(res);
+        });
+    });
 }
 
 export default {
